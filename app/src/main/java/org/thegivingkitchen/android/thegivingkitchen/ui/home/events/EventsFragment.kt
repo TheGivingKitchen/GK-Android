6e0b558e3ed572600d612319da5663d1ef0397ec
Shounak <shounakkul@gmail.com>
2018-12-05 20:22:34 -0500

    events tab header

--- app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/home/events/EventsFragment.kt
+++ app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/home/events/EventsFragment.kt
@@ -6,12 +6,25 @@ import android.support.v4.app.Fragment
 import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
+import kotlinx.android.synthetic.main.fragment_events.*
 import org.thegivingkitchen.android.thegivingkitchen.R
+import org.thegivingkitchen.android.thegivingkitchen.util.CustomTabs
 
 class EventsFragment : Fragment() {
+    private val learnMoreURL = "https://thegivingkitchen.org/events/"
+
     @Nullable
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_events, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        learn_more_button_eventsTab.setOnClickListener(learnMoreButtonClickListener)
+    }
+
+    private val learnMoreButtonClickListener = View.OnClickListener {
+        CustomTabs.openCustomTab(context, learnMoreURL)
     }
 }
