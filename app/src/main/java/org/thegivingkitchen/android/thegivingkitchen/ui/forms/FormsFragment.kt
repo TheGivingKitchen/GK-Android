7695075d0149b07acfd1a219074998ecf857de96
Shounak <shounakkul@gmail.com>
2019-01-08 20:05:31 -0500

    downloading and displaying correct forms json

--- app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/forms/FormsFragment.kt
+++ app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/forms/FormsFragment.kt
@@ -1,5 +1,7 @@
 package org.thegivingkitchen.android.thegivingkitchen.ui.forms
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
@@ -7,10 +9,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_forms.*
+import kotlinx.android.synthetic.main.fragment_safetynet.*
 import org.thegivingkitchen.android.thegivingkitchen.R
+import org.thegivingkitchen.android.thegivingkitchen.ui.home.safetynet.SafetynetViewModel
 import org.thegivingkitchen.android.thegivingkitchen.util.Constants.formsArg
+import org.thegivingkitchen.android.thegivingkitchen.util.Firebase
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
 
 class FormsFragment : Fragment() {
+    private lateinit var model: FormsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        model = ViewModelProviders.of(this).get(FormsViewModel::class.java)
+        model.getCurrentJson().observe(this, Observer<String> { liveData ->
+            updateJson(liveData!!)
+        })
+    }
 
     @Nullable
     override fun onCreateView(
@@ -20,6 +38,39 @@ class FormsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        textView_forms.text = arguments?.getString(formsArg)
+        // textView_forms.text = arguments?.getString(formsArg)
+        // todo: delete this file when done
+        val localFile = File.createTempFile("form", "json")
+        if (arguments != null) {
+            Firebase.firebaseInstance.getReferenceFromUrl(arguments!!.getString(formsArg))
+                    .getFile(localFile)
+                    .addOnSuccessListener {
+                        val stringBuilder = StringBuilder()
+//                         progressBar_safetynetTab.visibility = View.GONE
+                        try {
+                            val bufferedReader = BufferedReader(FileReader(localFile))
+                            var line = bufferedReader.readLine()
+                            while (line != null) {
+                                stringBuilder.append(line)
+                                line = bufferedReader.readLine()
+                            }
+                            bufferedReader.close()
+                            val jsonString = stringBuilder.toString()
+//                              val safetynetData = jsonAdapter.nullSafe().fromJson(jsonString)?.safetyNet
+ //                            if (safetynetData != null) {
+                                model.setCurrentJson(jsonString)
+//                             }
+                        } catch (e: IOException) {
+                            progressBar_safetynetTab.visibility = View.GONE
+                            // todo: log error
+                        }
+                    }.addOnFailureListener {
+                        // todo: log error and show error state
+                    }
+        }
+    }
+
+    private fun updateJson(data: String) {
+        textView_forms.text = data
     }
 }
--- app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/forms/FormsViewModel.kt
+++ app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/forms/FormsViewModel.kt
@@ -1,7 +1,17 @@
 package org.thegivingkitchen.android.thegivingkitchen.ui.forms
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 class FormsViewModel: ViewModel() {
+    private var currentJson: MutableLiveData<String> = MutableLiveData()
 
+    fun getCurrentJson(): LiveData<String> {
+        return currentJson
+    }
+
+    fun setCurrentJson(data: String) {
+        currentJson.value = data
+    }
 }
