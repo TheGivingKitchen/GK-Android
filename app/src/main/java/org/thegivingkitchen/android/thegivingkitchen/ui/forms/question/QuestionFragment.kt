9a0349288e1e713abaf6446acd70c4684cf8f79c
Shounak <shounakkul@gmail.com>
2019-01-23 20:04:07 -0500

    save date dialog date and pass it back into date dialog

--- app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/forms/question/QuestionFragment.kt
+++ app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/forms/question/QuestionFragment.kt
@@ -1,22 +1,29 @@
 package org.thegivingkitchen.android.thegivingkitchen.ui.forms.question
 
+import android.app.DatePickerDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.support.annotation.Nullable
+import android.support.v4.app.DialogFragment
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import kotlinx.android.synthetic.main.fragment_form_question.*
 import org.thegivingkitchen.android.thegivingkitchen.R
 import org.thegivingkitchen.android.thegivingkitchen.ui.forms.Page
-import org.thegivingkitchen.android.thegivingkitchen.ui.forms.questionviews.DateQuestion
-import org.thegivingkitchen.android.thegivingkitchen.ui.forms.questionviews.EmailQuestion
-import org.thegivingkitchen.android.thegivingkitchen.ui.forms.questionviews.FullnameQuestion
-import org.thegivingkitchen.android.thegivingkitchen.ui.forms.questionviews.PhoneQuestion
+import org.thegivingkitchen.android.thegivingkitchen.ui.forms.questionviews.*
 import org.thegivingkitchen.android.thegivingkitchen.util.BackPressedListener
 import org.thegivingkitchen.android.thegivingkitchen.util.setTextIfItExists
+import java.util.*
 
-class QuestionFragment: Fragment(), BackPressedListener {
+class QuestionFragment: Fragment(), BackPressedListener, DatePickerDialog.OnDateSetListener {
+    private var dateQuestion: DateQuestion? = null
+    private var dateYear: Int? = null
+    private var dateMonth: Int? = null
+    private var dateDay: Int? = null
+
     companion object {
         const val pageArg = "page"
 
@@ -52,10 +59,60 @@ class QuestionFragment: Fragment(), BackPressedListener {
         container_formQuestion.addView(FullnameQuestion("Question 1", context!!))
         container_formQuestion.addView(PhoneQuestion("Question 2", context!!))
         container_formQuestion.addView(EmailQuestion("Question 3", context!!))
-        container_formQuestion.addView(DateQuestion("Question 4", fragmentManager!!, context!!))
+
+        val dateQuestionView = DateQuestion("Question 4", context!!)
+        dateQuestion = dateQuestionView
+        container_formQuestion.addView(dateQuestion)
+        dateQuestion!!.setOnClickListener(dateViewClickListener)
     }
 
     override fun onBackPressed(): Boolean {
         return false
+    }
+
+    private val dateViewClickListener = View.OnClickListener {
+        val datePickerFragment = DatePickerFragment()
+        datePickerFragment.newInstance(this, dateYear, dateMonth, dateDay)
+        datePickerFragment.show(fragmentManager, "hello")
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        dateYear = year
+        dateMonth = month
+        dateDay = dayOfMonth
+
+        dateQuestion?.setDate(dateMonth!!, dateDay!!, dateYear!!)
+    }
+}
+
+class DatePickerFragment: DialogFragment() {
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+    private var year: Int = 0
+    private var month: Int = 0
+    private var day: Int = 0
+
+    fun newInstance(listener: DatePickerDialog.OnDateSetListener, selectedYear: Int? = null, selectedMonth: Int? = null, selectedDay: Int? = null) {
+        dateSetListener = listener
+
+        val calendar = Calendar.getInstance()
+        if (selectedYear != null) {
+            year = selectedYear
+        } else {
+            year = calendar.get(Calendar.YEAR)
+        }
+        if (selectedMonth != null) {
+            month = selectedMonth
+        } else {
+            month = calendar.get(Calendar.MONTH)
+        }
+        if (selectedDay != null) {
+            day = selectedDay
+        } else {
+            day = calendar.get(Calendar.DAY_OF_MONTH)
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return DatePickerDialog(context!!, dateSetListener, year, month, day)
     }
 }
--- app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/forms/questionviews/DateQuestion.kt
+++ app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/forms/questionviews/DateQuestion.kt
@@ -1,47 +1,28 @@
 package org.thegivingkitchen.android.thegivingkitchen.ui.forms.questionviews
 
-import android.annotation.TargetApi
-import android.app.DatePickerDialog
-import android.app.Dialog
 import android.content.Context
-import android.os.Bundle
-import android.support.v4.app.DialogFragment
-import android.support.v4.app.FragmentManager
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.DatePicker
 import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
 import kotlinx.android.synthetic.main.view_question_date.view.*
 import org.thegivingkitchen.android.thegivingkitchen.R
+import java.util.*
 
-// todo: stop passing in fragment manager here
-class DateQuestion(title: String?, fragmentManager: FragmentManager, context: Context, attrs: AttributeSet? = null, defStyle: Int = 0): LinearLayout(context, attrs, defStyle){
+class DateQuestion(title: String?, context: Context, attrs: AttributeSet? = null, defStyle: Int = 0): LinearLayout(context, attrs, defStyle){
     // todo: use merge tags in views
-    private val textViewClickListener = View.OnClickListener {
-        DatePickerFragment().show(fragmentManager, "hello")
-    }
-
     init {
         LayoutInflater.from(context).inflate(R.layout.view_question_date, this, true)
         layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         title_dateQuestion.text = title
-        setOnClickListener(textViewClickListener)
+        val calendar = Calendar.getInstance()
+        setDate(calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR))
+    }
+
+    fun setDate(month: Int, day: Int, year: Int) {
+        date_dateQuestion.text = context.getString(R.string.date_question_date, month+1, day, year)
     }
 }
 
-// todo: make this available to apis < 25
-@TargetApi(25)
-class DatePickerFragment: DialogFragment(), DatePickerDialog.OnDateSetListener {
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return DatePickerDialog(context!!)
-    }
 
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        // date_dateQuestion.text = context!!.getString(R.string.date_question_date, dayOfMonth, month, year)
-    }
-}
 
