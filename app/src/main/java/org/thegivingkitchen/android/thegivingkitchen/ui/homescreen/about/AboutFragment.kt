e3eafd4e0f86227d4d5597363de7aff33b62c29b
Shounak <shounakkul@gmail.com>
2019-02-19 21:18:08 -0500

    about tab ui

--- app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/homescreen/about/AboutFragment.kt
+++ app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/homescreen/about/AboutFragment.kt
@@ -1,7 +1,18 @@
 package org.thegivingkitchen.android.thegivingkitchen.ui.homescreen.about
 
+import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.thegivingkitchen.android.thegivingkitchen.R
 
 class AboutFragment : Fragment()  {
 
+    @Nullable
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
 }
