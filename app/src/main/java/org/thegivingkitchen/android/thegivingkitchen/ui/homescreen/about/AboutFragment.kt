4f69933028833d8ee7ceeb15ae678ee3d0d8092f
Shounak <shounakkul@gmail.com>
2019-02-19 21:49:35 -0500

    about tab click actions

--- app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/homescreen/about/AboutFragment.kt
+++ app/src/main/java/org/thegivingkitchen/android/thegivingkitchen/ui/homescreen/about/AboutFragment.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_about.*
 import org.thegivingkitchen.android.thegivingkitchen.R
 
 class AboutFragment : Fragment()  {
@@ -14,5 +16,52 @@ class AboutFragment : Fragment()  {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        aboutUsButton_aboutTab.setOnClickListener(aboutUsButtonClickListener)
+        newsletterSignup_aboutTab.setOnClickListener(newsletterSignupClickListener)
+        storyOneImage_aboutTab.setOnClickListener(storyOneClickListener)
+        storyOneLink_aboutTab.setOnClickListener(storyOneClickListener)
+        storyTwoImage_aboutTab.setOnClickListener(storyTwoClickListener)
+        storyTwoLink_aboutTab.setOnClickListener(storyTwoClickListener)
+        storyThreeImage_aboutTab.setOnClickListener(storyThreeClickListener)
+        storyThreeLink_aboutTab.setOnClickListener(storyThreeClickListener)
+        feedbackPositive_aboutTab.setOnClickListener(feedbackPositiveClickListener)
+        feedbackNeutral_aboutTab.setOnClickListener(feedbackNeutralClickListener)
+        feedbackNegative_aboutTab.setOnClickListener(feedbackNegativeClickListener)
+    }
+
+    private val aboutUsButtonClickListener = View.OnClickListener {
+        Toast.makeText(context, "How it works button clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private val newsletterSignupClickListener = View.OnClickListener {
+        Toast.makeText(context, "Newsletter signup clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private val storyOneClickListener = View.OnClickListener {
+        Toast.makeText(context, "Story One clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private val storyTwoClickListener = View.OnClickListener {
+        Toast.makeText(context, "Story Two clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private val storyThreeClickListener = View.OnClickListener {
+        Toast.makeText(context, "Story Three clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private val feedbackPositiveClickListener = View.OnClickListener {
+        Toast.makeText(context, "+ Feedback clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private val feedbackNeutralClickListener = View.OnClickListener {
+        Toast.makeText(context, "/ Feedback clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private val feedbackNegativeClickListener = View.OnClickListener {
+        Toast.makeText(context, "- Feedback clicked", Toast.LENGTH_SHORT).show()
     }
 }
