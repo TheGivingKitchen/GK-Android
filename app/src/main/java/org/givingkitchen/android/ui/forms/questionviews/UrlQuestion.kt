cb2a6465169bd7d704904f728f4c3e6a5ae67a14
Shounak <shounakkul@gmail.com>
2019-03-03 14:45:09 -0500

    merge tag for url question

--- app/src/main/java/org/givingkitchen/android/ui/forms/questionviews/UrlQuestion.kt
+++ app/src/main/java/org/givingkitchen/android/ui/forms/questionviews/UrlQuestion.kt
@@ -8,13 +8,16 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.view_question_url.view.*
 import org.givingkitchen.android.R
+import org.givingkitchen.android.util.convertToDp
 import org.givingkitchen.android.util.setTextIfItExists
 
 class UrlQuestion(title: String?, answer: String? = null, context: Context, attrs: AttributeSet? = null, defStyle: Int = 0): LinearLayout(context, attrs, defStyle), QuestionView {
     // todo: use merge tags in views
     init {
         LayoutInflater.from(context).inflate(R.layout.view_question_url, this, true)
-        layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val customLayoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        customLayoutParams.setMargins(0,0,0, convertToDp(20, resources))
+        layoutParams = customLayoutParams
         this.orientation = VERTICAL
         title_urlQuestion.setTextIfItExists(title)
         if (!answer.isNullOrBlank()) {
