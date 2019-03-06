1c68e0c29d074ae94147fcbaa41185b8b3de9655
Shounak <shounakkul@gmail.com>
2019-03-05 21:40:28 -0500

    how it works screen

--- app/src/main/java/org/givingkitchen/android/ui/homescreen/about/HowItWorksFragment.kt
+++ app/src/main/java/org/givingkitchen/android/ui/homescreen/about/HowItWorksFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_how_it_works.*
 import org.givingkitchen.android.R
 
 class HowItWorksFragment : Fragment() {
@@ -13,5 +15,11 @@ class HowItWorksFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_how_it_works, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        photoView_howItWorks.setImageResource(R.drawable.pic_how_it_works)
+        closeButton_howItWorks.setOnClickListener { findNavController().navigateUp() }
     }
 }
