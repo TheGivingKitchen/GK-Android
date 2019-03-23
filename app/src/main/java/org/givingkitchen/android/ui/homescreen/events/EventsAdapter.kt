3febd0b478b76cdcea4cafedd971c5d8b33258ff
Shounak <shounakkul@gmail.com>
2019-03-22 23:07:51 -0400

    convert give tab to all constraintlayout

--- app/src/main/java/org/givingkitchen/android/ui/homescreen/events/EventsAdapter.kt
+++ app/src/main/java/org/givingkitchen/android/ui/homescreen/events/EventsAdapter.kt
@@ -2,7 +2,6 @@ package org.givingkitchen.android.ui.homescreen.events
 
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
